2734fb4f6ff39cd4f636e0984671a7fc1448d9d0
hiltontj <trevor.hilton@gmail.com>
2016-08-07 15:08:43 -0400

    adaboost

--- train_test.py
+++ train_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from sklearn.metrics import auc, roc_curve
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -25,13 +25,14 @@ clf.fit(X_train, y_train)
 probas_ = clf.predict_proba(X_test)
 fpr, tpr, thresholds = roc_curve(y_test, probas_[:, 1])
 roc_auc = auc(fpr, tpr)
+print(roc_auc)
 
-plt.plot(fpr, tpr, lw=1, label='ROC Test (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
-plt.xlim([-0.05, 1.05])
-plt.ylim([-0.05, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC for %s, Number of Ensembles: %0.0d' % (state_switch, numest))
-plt.legend(loc="lower right")
-plt.show()
+# plt.plot(fpr, tpr, lw=1, label='ROC Test (area = %0.2f)' % roc_auc)
+# plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
+# plt.xlim([-0.05, 1.05])
+# plt.ylim([-0.05, 1.05])
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.title('ROC for %s, Number of Ensembles: %0.0d' % (state_switch, numest))
+# plt.legend(loc="lower right")
+# plt.show()
